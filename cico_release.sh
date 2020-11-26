5a9ced7daa8aa104126c0b55d4e49338d88c4c70
Mykhailo Kuznietsov <mkuznets@redhat.com>
2020-11-26 16:32:52 +0200

    Release 7.22.1

--- cico_release.sh
+++ cico_release.sh
@@ -484,12 +484,12 @@ set -e
 
 loginQuay
 
-#release che-theia, machine-exec and devfile-registry
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-theia            devtools-che-theia-che-release        90 & }; pid_1=$!;
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-machine-exec     devtools-che-machine-exec-release     60 & }; pid_2=$!;
- { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-devfile-registry devtools-che-devfile-registry-release 75 & }; pid_3=$!;
-wait
-# then release plugin-registry (depends on che-theia and machine-exec)
+# #release che-theia, machine-exec and devfile-registry
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-theia            devtools-che-theia-che-release        90 & }; pid_1=$!;
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-machine-exec     devtools-che-machine-exec-release     60 & }; pid_2=$!;
+#  { ./cico_release_theia_and_registries.sh ${CHE_VERSION} eclipse/che-devfile-registry devtools-che-devfile-registry-release 75 & }; pid_3=$!;
+# wait
+# # then release plugin-registry (depends on che-theia and machine-exec)
 
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-machine-exec:${CHE_VERSION} 5
 verifyContainerExistsWithTimeout ${REGISTRY}/${ORGANIZATION}/che-devfile-registry:${CHE_VERSION} 5
